2589351e7176bc8d516385c9cce3602d44e15cdd
esionascimento <esio.392@protonmail.com>
2021-12-21 16:12:41 -0400

    refatora arquivo de validacao de register

--- api/middlewares/validate/register.js
+++ api/middlewares/validate/register.js
@@ -2,10 +2,11 @@ const Joi = require('joi');
 
 const validateUser = (req, res, next) => {
   const { error } = Joi.object({
-    name: Joi.not().empty().required(),
-    email: Joi.not().empty().required(),
-    password: Joi.not().empty().required(),
-    idStore: Joi.not().empty().required(),
+    name: Joi.string().min(3).max(20).required(),
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    password: Joi.string()
+    .pattern(new RegExp('^([`~!@#$%^&*()\/a-zA-Z0-9 \t]{6,30})$')),
+    idStore: Joi.string().min(36).max(36).required()
   }).validate(req.body);
   
   if (error) return res.status(400).json({message: error.details[0].message});
