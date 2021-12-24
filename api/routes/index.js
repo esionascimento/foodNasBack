da75dfc867871f173341ee59013f519da48e753e
esionascimento <esio.392@protonmail.com>
2021-12-24 19:24:00 -0400

    rota para endpoint merchant/merchant/status

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,7 @@ const { routerUser } = require('../controllers/registerController');
 const { routerLogin } = require('../controllers/loginController');
 const { routerAuthentication} = require('../controllers/authenticationController')
 const { routerMerchantCatalog } = require('../controllers/merchantCatalogController')
+const { routerMerchant } = require('../controllers/merchantMerchantController')
 
 const routers = express.Router();
 
@@ -11,5 +12,6 @@ routers.use('/register', routerUser);
 routers.use('/login', routerLogin);
 routers.use('/authentication', routerAuthentication)
 routers.use('/merchant/catalog', routerMerchantCatalog)
+routers.use('/merchant/merchant', routerMerchant)
 
 module.exports = routers;
