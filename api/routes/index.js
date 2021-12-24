c2f684d3c9a43d67600ad80f76098fa8ef0180d1
esionascimento <esio.392@protonmail.com>
2021-12-24 16:12:40 -0400

    criado rota para /authentication

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const { routerUser } = require('../controllers/registerController');
 const { routerLogin } = require('../controllers/loginController');
+const { routerAuthentication} = require('../controllers/authenticationController')
 
 const routers = express.Router();
 
 routers.use('/register', routerUser);
 routers.use('/login', routerLogin);
+routers.use('/authentication', routerAuthentication)
 
 module.exports = routers;
