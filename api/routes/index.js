f9d48258a3d82d1486af88402149eca21794676a
esionascimento <esio.392@protonmail.com>
2021-12-24 18:51:47 -0400

    refatora rota para endpoint catalog

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const { routerUser } = require('../controllers/registerController');
 const { routerLogin } = require('../controllers/loginController');
 const { routerAuthentication} = require('../controllers/authenticationController')
+const { routerMerchantCatalog } = require('../controllers/merchantCatalogController')
 
 const routers = express.Router();
 
 routers.use('/register', routerUser);
 routers.use('/login', routerLogin);
 routers.use('/authentication', routerAuthentication)
+routers.use('/merchant/catalog', routerMerchantCatalog)
 
 module.exports = routers;
