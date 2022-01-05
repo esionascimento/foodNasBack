0dfc7e9ff5ab3a9ffc4534cf0b285659e49d3c78
esionascimento <esio.392@protonmail.com>
2022-01-05 15:01:45 -0400

    criado arquivo controller address

---
+++ api/controllers/AddressController.js
@@ -0,0 +1,27 @@
+const User = require('../models/UserModel');
+const Address = require('../models/AddressModel');
+
+module.exports = {
+  async store(req, res) {
+    const { user_id } = req.params;
+    const { zipcode, street, number } = req.body;
+
+    const user = await User.findByPk(user_id);
+
+    if(!user) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    try {
+      const address = await Address.create({
+        zipcode,
+        street,
+        number,
+        user_id
+      });
+      return res.json(address);
+    } catch(err) {
+      console.log('address :', err);
+    }
+  }
+};
