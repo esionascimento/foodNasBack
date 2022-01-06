43113a9a33e87c9f85f1f79661345c47824d11a9
esionascimento <esio.392@protonmail.com>
2022-01-06 16:56:54 -0400

    refatora LoginController captura de error

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,10 +1,14 @@
 const { LoginService } = require('../services/loginService');
 
 module.exports = {
-  async login(req, res) {
+  async login(req, res, next) {
     const resultLogin = await LoginService(req.body);
+    
+    if (resultLogin.isError) {
+      return next(resultLogin);
+    }
 
     const { id, name, email, id_store, token } = resultLogin;
-    return res.status(200).json({ id, name, email, id_store, token });
+    return res.status(200).json({id, name, email, id_store, token});
   }
 };
