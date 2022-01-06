9f7dd19f19afd63403df628b0a2ea304cb5a10a4
esionascimento <esio.392@protonmail.com>
2022-01-06 16:57:38 -0400

    nova rota /login | import LoginController

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-const { routerLogin } = require('../controllers/atlas/loginController');
 const { routerAuthentication} = require('../controllers/ifood/authenticationController');
 const { routerMerchantCatalog } = require('../controllers/ifood/merchantCatalogController');
 const { routerMerchant } = require('../controllers/ifood/merchantMerchantController');
@@ -9,10 +8,10 @@ const { routerAuth } = require('../controllers/atlas/authorizationController');
 
 const RegisterController = require('../controllers/RegisterController');
 const AddressController = require('../controllers/AddressController');
+const LoginController = require('../controllers/LoginController');
 
 const routers = express.Router();
 
-routers.use('/login', routerLogin);
 routers.use('/authorization', routerAuth);
 routers.use('/merchant/authentication', routerAuthentication);
 routers.use('/merchant/catalog', routerMerchantCatalog);
@@ -22,5 +21,6 @@ routers.use('/merchant/order', routerMerchantOrder);
 routers.post('/register', RegisterController.store);
 routers.get('/userss', RegisterController.index);
 routers.post('/users/:user_id/address', AddressController.store);
+routers.post('/login', LoginController.login);
 
 module.exports = routers;
