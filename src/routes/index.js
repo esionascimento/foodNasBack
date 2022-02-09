5f7fa93dfffdb953771c2ffe6dcf842fff1161c3
esionascimento <esio.392@protonmail.com>
2022-02-09 04:47:59 -0400

    criado endpoint post get /store_hours

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ const RegisterController = require('../controllers/postgresql/RegisterController
 const AddressController = require('../controllers/postgresql/AddressController');
 const LoginController = require('../controllers/postgresql/LoginController');
 const AuthorizationController = require('../controllers/postgresql/AuthorizationController');
+const StoreHoursController = require('../controllers/postgresql/StoreHoursController');
 
 const routers = express.Router();
 
@@ -18,6 +19,9 @@ routers.get('/register', RegisterController.index);
 routers.post('/register', RegisterController.store);
 routers.delete('/register', RegisterController.deleteAll);
 routers.put('/register/:user_id', RegisterController.updateOne);
+
+routers.post('/store_hours/:user_id', StoreHoursController.store);
+routers.get('/store_hours/:user_id', StoreHoursController.index);
 
 routers.post('/login', LoginController.login);
 
